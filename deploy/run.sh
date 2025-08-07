207926bae0bb67cb23b3a9e9cadc770ee6d9dda5
Nguyen Da <thanhda10cl@gmail.com>
2025-08-08 02:54:14 +0700

    update deploy

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,12 +3,15 @@ RUN_DIR={TARGET_DIR}
 MYAPP_CLASSPATH=$RUN_DIR/classes
 PATH_LIBS=$RUN_DIR/lib/*.jar
 LIBS=""
+
 for file in $PATH_LIBS
 do
   LIBS=$file:$LIBS
 done
+
 export JAVA_OPTS="-Djava.awt.headless=true -Dfile.encoding=UTF-8 -server -XX:+DisableExplicitGC"
 MYAPP_CLASSPATH=$MYAPP_CLASSPATH:$LIBS
-java -Xms1024m -Xmx2048m -cp $MYAPP_CLASSPATH  lf.video.Server
+
+/opt/jdk11/bin/java -Xms1024m -Xmx2048m -cp $MYAPP_CLASSPATH itcareer.Server
 
 echo "Application Socket Started"
